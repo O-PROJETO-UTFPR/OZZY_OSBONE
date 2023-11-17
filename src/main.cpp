d9e606fc15db99d551ad4a29d7e17ee25d1bdfd4
O Projeto <trocar.ideia.projeto@gmail.com>
2023-11-17 17:39:22 -0300

    ADD TWO MORE SERVOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,21 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-static const int servoPin = 25;
+static const int servoPin = 26; 
+static const int servoPinA = 25; 
+static const int servoPinB = 33; 
+
+
+
+//25, 33 
 int cont = 0;
 Servo servo1;
 
 void setup() {
     Serial.begin(9600);
     servo1.attach(servoPin);
+    servoA.attach(servoPinA);
+    servoB.attach(servoPinB);
     
 }
 
@@ -37,4 +45,29 @@ void loop() {
         delay(10);
     }
     cont = 0;
+
+
+    for(int posDegrees = 0; posDegrees <= 180; posDegrees++) {
+    servoA.write(posDegrees);
+    .println(posDegrees);
+    delay(20);
+    }
+
+    for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {
+        servoA.write(posDegrees);
+        Serial.println(posDegrees);
+        delay(20);
+    }
+
+       for(int posDegrees = 0; posDegrees <= 180; posDegrees++) {
+        servoB.write(posDegrees);
+        Serial.println(posDegrees);
+        delay(20);
+    }
+
+    for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {
+        servoB.write(posDegrees);
+        Serial.println(posDegrees);
+        delay(20);
+    }
 }
